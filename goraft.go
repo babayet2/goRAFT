884c6b00537c3950a95816da3d4b79cf0c1b2814
babayet2 <htrarabi@gmail.com>
2019-03-20 15:18:48 -0700

    refactored code and changed function names for readability

--- goraft.go
+++ goraft.go
@@ -17,6 +17,7 @@ import (
 
 type State int
 
+//enumerate possible states
 const (
 	follower State = 0
 	candidate State = 1
@@ -25,6 +26,7 @@ const (
 
 //store node information
 type Node struct {
+	//number of nodes in the network
 	nodes int
 	pid int
 	term int
@@ -35,11 +37,43 @@ type Node struct {
 	clients [256]*rpc.Client
 }
 
-func (n *Node) log(str string){
-	fmt.Printf("[PID %03d | TERM %03d]  %s\n", n.pid, n.term, str);
+//initialize a node with values passed via command line arguments
+func init_node() *Node {
+	n := new(Node)
+	i, err := strconv.Atoi(os.Args[1])
+	n.nodes = i
+	if err!=nil{
+		log.Fatal("error parsing command line arguments", err)
+	}
+	i, err = strconv.Atoi(os.Args[2])
+	n.pid = i
+	if err!=nil {
+		log.Fatal("error parsing command line arguments", err)
+	}
+	return n
 }
 
-//does not have to be exported
+//simple logging funciton, write time/term/pid in each log entry
+func (n *Node) log(str string){
+	time := time.Now()
+	fmt.Printf("[%s | PID %03d | TERM %03d]  %s\n", time.Format("15:04:05.0000"), n.pid, n.term, str);
+}
+
+//start listening for connections from other nodes
+func (n *Node) init_server(){
+	rpc.Register(n)
+	rpc.HandleHTTP()
+	l, err := net.Listen("tcp", ":" + strconv.Itoa(1200 + n.pid))
+	if err != nil {
+		n.log("listen error")
+	}
+	n.log("serving RPC server on port: " + strconv.Itoa(1200 + n.pid))
+	// Start accept incoming HTTP connections
+	go http.Serve(l, nil)
+
+}
+
+//create connections to each other node in the network, and store their information in the clients array
 func (n *Node) init_channels(){
 	for i:=0; i<n.nodes; i++ {
 		//attempt to initiate connections with each client
@@ -56,20 +90,7 @@ func (n *Node) init_channels(){
 	return
 }
 
-//does not have to be exported
-func (n *Node) init_server(){
-	rpc.Register(n)
-	rpc.HandleHTTP()
-	l, err := net.Listen("tcp", ":" + strconv.Itoa(1200 + n.pid))
-	if err != nil {
-		n.log("listen error")
-	}
-	n.log("Serving RPC server on port: " + strconv.Itoa(1200 + n.pid))
-	// Start accept incoming HTTP connections
-	go http.Serve(l, nil)
-
-}
-
+//receive a vote request from another node. if its term is higher, update this node's term and send a vote
 func (n *Node) Receive_vote_request(term int, reply *int) error{
 	//fmt.Println(term, n.term)
 	n.last_com = time.Now()
@@ -78,6 +99,7 @@ func (n *Node) Receive_vote_request(term int, reply *int) error{
 			n.state = follower
 		}
 		n.term = term
+		//send a vote
 		*reply = 1
 		return nil
 	}
@@ -85,7 +107,20 @@ func (n *Node) Receive_vote_request(term int, reply *int) error{
 	return nil
 }
 
+//request a vote from another node by pid, calling "receive_vote_request" on that node via rpc
+func (n *Node) send_vote_request(pid int) int{
+	var reply int
+	err := n.clients[pid].Call("Node.Receive_vote_request", n.term, &reply)
+	if err!=nil{
+		//uncomment the line below if crashes won't be simulated
+		//n.log("failed to receive response from PID " + strconv.Itoa(pid))
+	}
+	return reply
+}
+
+//receive a heartbeat from the leader, and update my last_com time
 func (n *Node) Receive_heartbeat(term int, reply *int) error{
+	//update last_com time to now
 	n.last_com = time.Now()
 	if term>n.term{
 		n.term = term
@@ -97,39 +132,28 @@ func (n *Node) Receive_heartbeat(term int, reply *int) error{
 	return nil
 }
 
-func (n *Node) request_vote(pid int) int{
-	var reply int
-	err := n.clients[pid].Call("Node.Receive_vote_request", n.term, &reply)
-	if err!=nil{
-		//log.Fatal("vote request:", err)
-	}
-	return reply
-}
-
+//send a heartbeat to another node by pid, calling "Receive_heartbeat" via rpc
 func (n *Node) send_heartbeat(pid int) int{
 	var reply int
 	err := n.clients[pid].Call("Node.Receive_heartbeat", n.term, &reply)
 	if err!=nil{
-		//log.Fatal("heartbeat request:", err)
+		//uncomment the line below if crashes won't be simulated
+		//n.log("failed to receive response from PID " + strconv.Itoa(pid))
 	}
 	return reply
 }
 
+//send heartbeats to all nodes concurrently
 func (n *Node) send_heartbeats(){
 	for i:=0;i<n.nodes;i++{
 		go n.send_heartbeat(i)
 	}
 }
 
-func (n *Node) leader_crash(){
-	time.Sleep(time.Second * 6)
-	n.log("simulated crash, killing process");
-	//log.Fatal("[PID " + strconv.Itoa(n.pid) + "] simulated crash, killing process");
-	os.Exit(0)
-}
 
-func (n *Node) Receive_leader_elected(term int, reply *int) error{
-	if term>n.term{
+//receive a message that another node has been elected leader
+func (n *Node) Receive_election_result(term int, reply *int) error{
+	if term>=n.term{
 		n.term = term
 		n.state = follower
 		n.last_com = time.Now()
@@ -138,16 +162,26 @@ func (n *Node) Receive_leader_elected(term int, reply *int) error{
 	return nil
 }
 
-func (n *Node) broadcast_election(){
+//broadcast election results to each node concurrently, by calling "Receive_election_result" on all other
+//nodes via rpc
+func (n *Node) send_election_result(){
 	var reply int
 	for i:=0;i<n.nodes;i++{
 		if i==n.pid{
 			continue
 		}
-		go n.clients[i].Call("Node.Receive_leader_elected", n.term, &reply)
+		go n.clients[i].Call("Node.Receive_election_result", n.term, &reply)
 	}
 }
 
+//simulate a crash by killing this node
+func (n *Node) leader_crash(){
+	time.Sleep(time.Second * 6)
+	n.log("simulated crash, killing process");
+	os.Exit(0)
+}
+
+//loop through possible states infinitely
 func (n *Node) state_loop(){
 	//inifite loop
 	for{
@@ -165,22 +199,22 @@ func (n *Node) state_loop(){
 			n.log("election timeout! becoming candidate")
 			stored_term := n.term
 			for i:=0;i<n.nodes;i++{
-				sum += n.request_vote(i)
+				sum += n.send_vote_request(i)
 			}
 			if(n.term==stored_term){
-				n.log("recieved " + strconv.Itoa(sum) + " votes")
+				n.log("received " + strconv.Itoa(sum) + " votes")
 				if sum>n.nodes/2{
 					n.state = leader
 					go n.leader_crash()
-					n.log("recieved vote majority! becoming leader")
-					n.broadcast_election()
+					n.log("received vote majority! becoming leader")
+					n.send_election_result()
 					continue
 				}
 			}
 			n.state = follower
 		}
 		if n.state==leader{
-			n.log("broadcasting heartbeats")	
+			n.log("broadcasting heartbeats")
 			n.send_heartbeats()
 			n.last_com = time.Now()
 			time.Sleep(time.Millisecond * 30)
@@ -188,19 +222,9 @@ func (n *Node) state_loop(){
 	}
 }
 
+
 func main(){
-	n := new(Node)
-	
-	i, err := strconv.Atoi(os.Args[1])
-	n.nodes = i
-	if err!=nil{ 
-		log.Fatal("error parsing command line arguments", err)
-	}
-	i, err = strconv.Atoi(os.Args[2])
-	n.pid = i
-	if err!=nil {
-		log.Fatal("error parsing command line arguments", err)
-	}
+	n := init_node()
 	n.init_server()
 	n.init_channels()
 	n.log("sucessfully initialized server and connected to all channels")
@@ -209,9 +233,4 @@ func main(){
 	n.election_timeout = time.Duration(rand.Intn(150) + 150) * time.Millisecond
 	//fmt.Println("[PID " + strconv.Itoa(n.pid) + "] election timeout: ", n.election_timeout)
 	n.state_loop()
-	
-	//stop server from closing immediately
-	var input string
-	fmt.Scanln(&input)
-	
 }
--- raft.sh
+++ raft.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-pkill -f goraft
+pkill -f goraft > /dev/null
 rm log.txt goraft
 go build goraft.go
 x=$1
@@ -7,7 +7,7 @@ if [ $# -eq 0 ]
 	then
 		x=10
 fi
-for ((i=0;i<=$x-1;i++))
+for ((i=0;i<$x;i++))
 do
 	echo "starting node $i"
 	./goraft $x $i >> log.txt &
@@ -16,5 +16,11 @@ echo "please wait while raft protocol is simulated"
 sleep 30
 echo "ending simulation, edit the sleep statement in raft.sh if you'd like the simulation to run longer"
 echo "see log in log.txt"
+
+#kill all goraft processes, and surpress error output from pkill
+exec 3>&2
+exec 2> /dev/null
 pkill -f goraft
+exec 2>&3
+
 echo "simulation complete" >> log.txt
